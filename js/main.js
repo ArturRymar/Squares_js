3ad9707be15373a2337eabe18bbe770a4324dc56
ArturRymar <artur.rimar25@gmail.com>
2019-07-03 19:12:31 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,132 +1,78 @@
-'use strict';
-class Box {
-    constructor(wrapper){
-        this.wrapper = wrapper;
-        this.table = wrapper.getElementsByClassName('main-table')[0];
-        this.addColBtn = wrapper.getElementsByClassName('add-col')[0];
-        this.addRowBtn = wrapper.getElementsByClassName('add-row')[0];
-        this.remRowBtn = this.wrapper.getElementsByClassName('but-hid2')[0];
-        this.remColBtn = this.wrapper.getElementsByClassName('but-hid1')[0];
-        this.addColBtn.addEventListener('click', this.addCol);
-        this.addRowBtn.addEventListener('click', this.addRow);
-        this.table.addEventListener('mouseover', this.showRemoveBtn);
-        this.remRowBtn.addEventListener('mouseover', this.remRow);
-        this.remColBtn.addEventListener('mouseover', this.remCol);
-        this.wrapper.children[0].addEventListener('mouseout', this.hidRemoveBtn);
+
+function init() {
+
+  class Box {
+    constructor(wrapper) {
+      this.wrapper = wrapper;
+      this.table = wrapper.getElementsByClassName('main-table')[0];
+      this.addColBtn = wrapper.getElementsByClassName('add-col')[0];
+      this.addRowBtn = wrapper.getElementsByClassName('add-row')[0];
+      this.remRowBtn = this.wrapper.getElementsByClassName('del-btn2')[0];
+      this.remColBtn = this.wrapper.getElementsByClassName('del-btn1')[0];
+      this.addColBtn.addEventListener('click', this.addCol);
+      this.addRowBtn.addEventListener('click', this.addRow);
+      this.table.addEventListener('mouseover', this.getHoveredItemCoords);
+      this.remRowBtn.addEventListener('click', this.removeRow);
+      this.remColBtn.addEventListener('click', this.removeCol);
+      this.wrapper.addEventListener('mouseout', this.hideRemoveBtn);
+      this.colIndex = null;
+      this.rowIndex = null;
     };
 
     addCol = () => {
-        const rows = this.table.getElementsByTagName('tr');
-        for (let row of rows) {
-            row.insertCell(-1);
-        }
+      Array
+        .from(this.table.rows)
+        .forEach(row => row.insertCell(-1));
     };
 
     addRow = () => {
-        const newRow = this.table.insertRow(-1);
-        const tr = this.table.querySelector('TR');
-        const tdSum = tr.getElementsByTagName('td');
-        for (let i = 0; i < tdSum.length; i++) {
-            newRow.insertCell(-1);
-        }
+      const newRow = this.table.insertRow(-1);
+      Array
+        .from(this.table.rows[0].cells)
+        .forEach(cell => newRow.insertCell(cell));
     };
 
-    showRemoveBtn = ({target}) => {
-        const coords = target.getBoundingClientRect();
-        const elem = document.elementFromPoint(coords.left, coords.top);
-        this.remColBtn.classList.add('visible');
-        this.remColBtn.style.left = coords.left + elem.clientWidth/2 - this.remColBtn.clientWidth/2 + pageXOffset  + 'px';
-        this.remRowBtn.classList.add('visible');
-        this.remRowBtn.style.top = coords.top + elem.clientHeight/2 - this.remRowBtn.clientHeight/2 +pageYOffset + 'px';
-        if (target === this.table) {
-            this.remRowBtn.classList.remove('visible');
-            this.remColBtn.classList.remove('visible');
-        }
-        const tr = this.table.querySelector('tr');
-        if (tr.children.length === 1) this.remColBtn.classList.remove('visible');
-        const trArr = this.table.getElementsByTagName('tr');
-        if(trArr.length === 1) this.remRowBtn.classList.remove('visible');
+    getHoveredItemCoords = ({ target }) => {
+      this.remColBtn.classList.add('visible');
+      this.remRowBtn.classList.add('visible');
+      const coords = target.getBoundingClientRect();
+      if (target.tagName === 'TD') {
+        this.colIndex = target.cellIndex;
+        this.rowIndex = target.parentNode.rowIndex;
+        this.remColBtn.style.left = coords.left + target.clientWidth / 2 - this.remColBtn.clientWidth / 2  + pageXOffset + 'px';
+        this.remRowBtn.style.top = coords.top + target.clientHeight / 2 - this.remRowBtn.clientHeight / 2 + pageYOffset + 'px';
+      }
+      this.checkHide();
     };
 
-    hidRemoveBtn = ({target, relatedTarget}) => {
-         if(relatedTarget !== this.remRowBtn && relatedTarget !== this.remColBtn) {
-                 this.remRowBtn.classList.remove('visible');
-                 this.remColBtn.classList.remove('visible');
-         }
-    };
-
-    remRow = ({target,relatedTarget}) => {
-        const coords = relatedTarget.getBoundingClientRect();
-        const elem = document.elementFromPoint(coords.left, coords.top);
-        target.onclick = () => {
-            elem.parentElement.parentElement.removeChild(elem.parentElement);
-            this.remRowBtn.classList.remove('visible');
-            this.remColBtn.classList.remove('visible');
-        }
-    };
-
-    remCol = ({target, relatedTarget}) => {
-        const coords = relatedTarget.getBoundingClientRect();
-        const trArr = this.table.getElementsByTagName('tr');
-        target.onclick = () => {
-            for (let i = 0; i < trArr.length; i++) {
-                for (let j = 0; j < trArr[i].children.length; j++) {
-                    const tdArr = trArr[i].getElementsByTagName('td');
-                    const tdCoords = tdArr[j].getBoundingClientRect();
-                    if (tdCoords.left === coords.left ) {
-                        tdArr[j].parentElement.removeChild(tdArr[j]);
-                        break;
-                    }
-                }
-            }
+    hideRemoveBtn = ({ target, relatedTarget }) => {
+      if (relatedTarget !== this.remRowBtn && relatedTarget !== this.remColBtn) {
         this.remRowBtn.classList.remove('visible');
         this.remColBtn.classList.remove('visible');
-        }
+      }
     };
 
+    removeRow = () => {
+      if (this.table.rows.length === 1) return;
+      this.table.deleteRow(this.rowIndex);
+      this.checkHide();
+    };
+
+    removeCol = () => {
+      if (this.table.rows[0].cells.length === 1) return;
+      Array
+        .from(this.table.rows)
+        .forEach(row => row.deleteCell(this.colIndex));
+      this.checkHide();
+    };
+
+    checkHide = () => {
+        const rows = this.table.rows;
+        if (rows[0].cells.length === 1) this.remColBtn.classList.remove('visible');
+        if (rows.length === 1) this.remRowBtn.classList.remove('visible');
+    }
+  }
+
+  Array.from(document.getElementsByClassName('wrapper')).map(elem => new Box(elem));
 }
-new Box(document.getElementsByClassName('wrapper')[0]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener('DOMContentLoaded', init);
